12a1362925d758f32f501c876ca3f537cb21b2f0
FB2018bot <44780450+FB2018bot@users.noreply.github.com>
2018-11-05 20:23:37 +0400

    Add files via upload

---
+++ komutlar/bilgi.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Bot sürümü: v' + ayarlar.surum + ' Yapımcı: Mehmet (FearlessBoy) **Sohbet ve Oyun**\n\n_**BOTUe=b EKLEMEK İÇİN LİNK:**_\n\nhttps://discordapp.com/oauth2/authorize?client_id=500919380591378433&scope=bot&permissions=805314622 \n\n_**Linkler:**_\n\n**Sohbet ve Oyun** sunucusunun davet linki: https://discord.gg/zz8GAdM n\nBotun davet linki: https://discordapp.com/oauth2/authorize?client_id=288310817810546699&scope=bot&permissions=401812495 \n\n**:copyright: 2018 Sohbet ve Oyun**');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['bot bilgi', 'botbilgi', 'bb', 'botb', 'bbot', 'hakkında', 'bot hakkında', 'bothakkında'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'bilgi',
+  description: 'Bot ile ilgili bilgi verir.',
+  usage: 'bilgi'
+};
